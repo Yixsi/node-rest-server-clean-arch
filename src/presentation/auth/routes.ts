c0049bf5023174be7b1916363c120251a93f0e75
Yixsi <yixsi3103@gmail.com>
2024-01-28 22:30:50 -0500

    feat: add dependency injection to auth controller

--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { AuthController } from "./controller";
+import { AuthDatasourceImpl, AuthRepositoryImpl } from "../../infrastructure";
 
 export class AuthRoutes {
   static get routes(): Router {
     // use static method if we don't have to do DI (Dependency Injection)
     const router = Router();
-    const controller = new AuthController();
+
+    const datasource = new AuthDatasourceImpl(); // the DB we are using
+    const authRepository = new AuthRepositoryImpl(datasource);
+    const controller = new AuthController(authRepository);
 
     router.post("/register", controller.registerUser);
 
