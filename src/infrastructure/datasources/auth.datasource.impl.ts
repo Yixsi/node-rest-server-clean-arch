ceb00156c02438419dc2e4974ecda5ae45b9e8ca
Yixsi <yixsi3103@gmail.com>
2024-02-09 23:41:02 -0500

    add user mapper to entity and handle error

--- src/infrastructure/datasources/auth.datasource.impl.ts
+++ src/infrastructure/datasources/auth.datasource.impl.ts
@@ -6,24 +6,34 @@ import {
   RegisterUserDto,
   User,
 } from "../../domain";
+import { UserMapper } from "../mappers/user.mapper";
+
+type HashFunction = (password: string) => string;
+type CompareFunction = (password: string, hashedPassword: string) => boolean;
 
 export class AuthDatasourceImpl implements AuthDatasource {
+  // dependency injection - this allows us to change the functions easily or define default functions
+  constructor(
+    private readonly hash: HashFunction = BcryptAdapter.hash,
+    private readonly compare: CompareFunction = BcryptAdapter.compare
+  ) {}
+
   async register(registerUserDto: RegisterUserDto): Promise<User> {
     const { name, email, password } = registerUserDto;
     try {
       const emailExists = await UserModel.findOne({ email });
-      if (emailExists) throw CustomError.badRequest("User already exists");
+      if (emailExists) throw CustomError.badRequest("Something went wrong");
 
       // hash password
       const user = await UserModel.create({
         name,
         email,
-        password: BcryptAdapter.hash(password),
+        password: this.hash(password),
       });
       await user.save();
 
       // map the response
-      return new User(user.id, name, email, password, ["ADMIN"]);
+      return UserMapper.userEntityFromObject(user);
     } catch (error) {
       if (error instanceof CustomError) throw error;
 
